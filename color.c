2351f3f13bf70a989dc4a4a9b7393d26390afb32
michaeldel <michaeldel@protonmail.com>
2021-11-25 17:49:28 +0100

    Refactor initializations, destructions, mainloop and updates

--- color.c
+++ color.c
@@ -21,10 +21,14 @@ unsigned long parsecolor(const char * string) {
     return strtol(&string[1], NULL, 16);
 }
 
-int main(int argc, char * argv[]) {
-    if (argc != 2) die("argument missing"); 
-    const unsigned long color = parsecolor(argv[1]);
+struct winstate {
+    Display * display;
+    int screen;
+    Window  window;
+    Atom WM_DELETE;
+};
 
+struct winstate initwinstate(unsigned long color) {
     Display * display = XOpenDisplay(NULL);
     if (display == NULL) die("failed to open display");
 
@@ -41,56 +45,96 @@ int main(int argc, char * argv[]) {
     Atom WM_DELETE = XInternAtom(display, "WM_DELETE_WINDOW", false);
     XSetWMProtocols(display, window, &WM_DELETE, 1);
 
-    cairo_surface_t * surface = cairo_xlib_surface_create(
-        display, window, DefaultVisual(display, screen), WIDTH, HEIGHT
-    );
-    cairo_t * ctx = cairo_create(surface);
+    struct winstate ws = {
+        .display = display,
+        .screen = screen,
+        .window = window,
+        .WM_DELETE = WM_DELETE,
+    };
+    return ws;
+}
 
+void destroywinstate(struct winstate ws) {
+    XDestroyWindow(ws.display, ws.window);
+    XCloseDisplay(ws.display);
+}
+
+struct drawstate {
+    cairo_surface_t * surface;
+    PangoFontDescription * fontdesc;
+    const char * text;
+};
+
+struct drawstate initdrawstate(struct winstate ws, const char * text) {
+    struct drawstate ds = {
+        .surface = cairo_xlib_surface_create(
+            ws.display, ws.window, DefaultVisual(ws.display, ws.screen), WIDTH, HEIGHT
+        ),
+       .fontdesc = pango_font_description_from_string("monospace"),
+       .text = text
+    };
+    return ds;
+}
+
+void destroydrawstate(struct drawstate ds) {
+    pango_font_description_free(ds.fontdesc);
+    cairo_surface_destroy(ds.surface);
+}
+
+void draw(struct drawstate ds) {
+    cairo_t * ctx = cairo_create(ds.surface);
+
+    /* TODO: should layout be moved into drawstate ? */
     PangoLayout * layout = pango_cairo_create_layout(ctx);
-    PangoFontDescription * fontdesc = pango_font_description_from_string("monospace");
+    pango_layout_set_font_description(layout, ds.fontdesc);
+    pango_layout_set_text(layout, ds.text, -1);
 
-    pango_layout_set_text(layout, argv[1], -1);
-    pango_layout_set_font_description(layout, fontdesc);
-    pango_font_description_free(fontdesc);
+    const int surfacewidth = cairo_xlib_surface_get_width(ds.surface);
+    const int surfaceheight = cairo_xlib_surface_get_height(ds.surface);
 
-    bool running = true;
+    int layoutwidth, layoutheight;
+    pango_layout_get_size(layout, &layoutwidth, &layoutheight);
+    cairo_move_to(
+        ctx,
+        (surfacewidth - (double) layoutwidth / PANGO_SCALE) / 2.0,
+        (surfaceheight - (double) layoutheight / PANGO_SCALE) / 2.0
+    );
+    pango_cairo_show_layout(ctx, layout);
 
-    while (running) {
+    cairo_destroy(ctx);
+}
+
+void resize(struct drawstate ds, int width, int height) {
+    cairo_xlib_surface_set_size(ds.surface, width, height);
+}
+
+void mainloop(struct winstate ws, struct drawstate ds) {
+    for (;;) {
         XEvent event;
-        XNextEvent(display, &event);
+        XNextEvent(ws.display, &event);
 
         switch(event.type) {
-        case Expose: {
-            const int surfacewidth = cairo_xlib_surface_get_width(surface);
-            const int surfaceheight = cairo_xlib_surface_get_height(surface);
-
-            int layoutwidth, layoutheight;
-            pango_layout_get_size(layout, &layoutwidth, &layoutheight);
-            cairo_move_to(
-                ctx,
-                (surfacewidth - (double) layoutwidth / PANGO_SCALE) / 2.0,
-                (surfaceheight - (double) layoutheight / PANGO_SCALE) / 2.0
-            );
-            pango_cairo_show_layout(ctx, layout);
-        }; break;
+        case Expose: draw(ds); break;
         case ConfigureNotify:
-            cairo_xlib_surface_set_size(
-                surface,
-                event.xconfigure.width,
-                event.xconfigure.height
-            );
+            resize(ds, event.xconfigure.width, event.xconfigure.height);
             break;
         case ClientMessage:
-            if ((Atom) event.xclient.data.l[0] == WM_DELETE) running = false;
+            if ((Atom) event.xclient.data.l[0] == ws.WM_DELETE) return;
             break;
         }
     }
+}
 
-    cairo_destroy(ctx);
-    cairo_surface_destroy(surface);
+int main(int argc, char * argv[]) {
+    if (argc != 2) die("argument missing"); 
+    const unsigned long color = parsecolor(argv[1]);
 
-    XDestroyWindow(display, window);
-    XCloseDisplay(display);
+    struct winstate ws = initwinstate(color);
+    struct drawstate ds = initdrawstate(ws, argv[1]);
+    mainloop(ws, ds);
+
+    destroydrawstate(ds);
+    destroywinstate(ws);
 
     return EXIT_SUCCESS;
 }
